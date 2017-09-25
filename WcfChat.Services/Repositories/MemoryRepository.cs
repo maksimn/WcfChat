75ff9a2273979cc7aade6b59048cb6b86d1baa2e
maksimn <maksim1989sovunion@yandex.ru>
2017-09-25 21:34:32 +0300

    MemoryRepository class

--- WcfChat.Services/Repositories/MemoryRepository.cs
+++ WcfChat.Services/Repositories/MemoryRepository.cs
@@ -1,40 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data;
 using WcfChat.Services.Repositories.InputModel;
 using WcfChat.Services.Repositories.Model;
 
 namespace WcfChat.Services.Repositories {
     class MemoryRepository : IUserRepository, IMessageRepository {
+        private static List<User> _users = new List<User>();
+        private static List<ChatMessage> _messages = new List<ChatMessage>();
+
         public IEnumerable<ChatMessage> ChatMessages {
             get {
-                throw new NotImplementedException();
+                return _messages;
             }
         }
 
         public int UserCount {
             get {
-                throw new NotImplementedException();
+                return _users.Count;
             }
         }
 
-        public void AddChatMessage(ChatDataInput chatMessage) {
-            throw new NotImplementedException();
-        }
-
         public void AddUser(UserRegistrationInput input) {
-            throw new NotImplementedException();
+            var userName = input.UserName;
+            var password = input.Password;
+
+            if (_users.Find(u => u.Name == userName) != null) {
+                throw new DuplicateNameException();
+            }
+            int newId = _users.Count;
+            var newUser = new User() { Id = newId, Name = userName, PasswordHash = password };
+
+            _users.Add(newUser);
         }
 
         public void Clear() {
-            throw new NotImplementedException();
+            _users.Clear();
         }
 
         public User GetUserByName(string userName) {
-            throw new NotImplementedException();
+            return _users.Find(u => u.Name == userName);
         }
 
         public bool LoginUser(string userName, string password) {
-            throw new NotImplementedException();
+            var user = GetUserByName(userName);
+
+            return user != null && user.PasswordHash == password;
+        }
+
+        public void AddChatMessage(ChatDataInput chatMessage) {
+            var message = new ChatMessage() {
+                Id = _messages.Count, Text = chatMessage.Text, UserName = chatMessage.UserName
+            };
+            _messages.Add(message);
         }
     }
 }
