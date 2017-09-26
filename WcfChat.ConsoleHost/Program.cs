e6d591c6ef8a4f94728bd652d8ace1a993c8cec6
maksimn <maksim1989sovunion@yandex.ru>
2017-09-26 04:32:37 +0300

    Wcf Chat Console Host

---
+++ WcfChat.ConsoleHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+using WcfChat.Services;
+
+namespace WcfChat.ConsoleHost {
+    class Program {
+        static void Main(string[] args) {
+            var hostAuthService = new ServiceHost(typeof(AuthService));
+            var hostChatService = new ServiceHost(typeof(ChatService));
+
+            hostAuthService.Open();
+            hostChatService.Open();
+
+            Console.WriteLine("Services started. Press [Enter] to exit.");
+            Console.ReadLine();
+
+            hostAuthService.Close();
+            hostChatService.Close();
+        }
+    }
+}
